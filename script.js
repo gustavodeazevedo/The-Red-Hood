51fe1f5610d62f38ce96fae9a4304cd21b33b60d
Gustavo de Azevedo <111658728+gustavodeazevedo@users.noreply.github.com>
2023-06-01 16:44:06 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+function home() {
+    window.location.href='#esquerdaMain'
+}
+
+function galeria() {
+    window.location.href='#galeriaSection'
+}
+
+function elementos() {
+    window.location.href='#elementosSection'
+}
+
+function faq() {
+    window.location.href='#faqSection'
+}
+
+function download() {
+    window.location.href='#downloadSection'
+}
